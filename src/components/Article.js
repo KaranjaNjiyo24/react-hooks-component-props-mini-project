8a0f2f5b301943906b690f15d3056fd35980bfd9
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2024-11-05 09:44:11 +0300

    Adds getemoji function

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,14 +1,29 @@
 import React from "react"
 
-function Article({title, date="January 1, 1970", preview }) {
+function Article({title, date="January 1, 1970", preview, minutes }) {
+
+    //Helper function 
+    const getEmoji = () => {
+        let emoji = ""
+        if (minutes < 30) {
+            const cups = Math.ceil(minutes /5)
+            emoji = "☕️".repeat(cups)
+        } else {
+            const bentos = Math.ceil(minutes /10)
+            emoji = "🍱".repeat(bentos)
+        }
+        return `${emoji} ${minutes} min read`
+    }
+
     return (
         <article>
             <h3>{title}</h3>
-            <small>{date}</small>
+            <small>{date} • {getEmoji()}</small>
             <p>{preview}</p>
         </article>
 
     )
 }
+
 
 export default Article;
