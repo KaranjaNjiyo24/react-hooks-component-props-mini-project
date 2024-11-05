a9487c52bf3a584ca590b830bfdb90317a95028f
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2024-11-05 05:29:22 +0300

    Updated the ArticleList.js, About.js

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,6 +1,20 @@
 import React from "react"
 
+function ArticleList() {
+    return (
+        <main>
+            {postMessage.map((post) =>(
+                <Article
+                    key={post.id}
+                    title={post.title}
+                    date={post.date}
+                    preview={post.preview}
+                />
+            ))}
+        </main>
+    )
 
+}
 
 
 export default ArticleList;
